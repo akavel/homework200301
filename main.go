787680b9b48bb9f8077aa74c7931d9ec324a25c9
Mateusz Czapliński <czapkofan@gmail.com>
2020-03-28 12:27:51 +0100

    sketch type User & mock data

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -19,6 +20,19 @@ func main() {
 	r.Methods("POST").Path("/v1/user").HandlerFunc(createUser)
 	r.Methods("PUT").Path("/v1/user/{id}").HandlerFunc(modifyUser)
 	log.Fatal(http.ListenAndServe(*addr, r))
+}
+
+type User struct {
+	Name    string
+	Surname string
+	Email   string
+	// FIXME: only store a hash of the password
+	Password   string
+	Birthday   time.Time
+	Address    string
+	Phone      *string
+	Technology string
+	Deleted    *time.Time
 }
 
 func listUsers(w http.ResponseWriter, r *http.Request) {
@@ -37,3 +51,67 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 func modifyUser(w http.ResponseWriter, r *http.Request) {
 	panic("NIY")
 }
+
+var mockUsers = []User{
+	{
+		Name: "John", Surname: "Smith",
+		Email:    "john@smith.name",
+		Password: "iAmJohnny",
+		// FIXME: provide location
+		Birthday:   time.Date(1980, 1, 1, 0, 0, 0, 0, nil),
+		Address:    "some city, some street 1",
+		Phone:      nil,
+		Technology: "php",
+		Deleted:    nil,
+	},
+	{
+		Name: "Anne J. Marie", Surname: "von Flick",
+		Email:    "Anne-Marie@vonflick.de",
+		Password: "foo123!%",
+		// FIXME: provide location
+		Birthday:   time.Date(2000, 12, 12, 0, 0, 0, 0, nil),
+		Address:    "Anneville",
+		Phone:      newString("+48 123-456-789"),
+		Technology: "java",
+		Deleted:    nil,
+	},
+	{
+		Name:     "Robert'); DROP TABLE Users;--",
+		Surname:  "Tables",                // TODO: fun surname (Unicode? Zalgo?)
+		Email:    "bobby.tables@xkcd.com", // TODO: fun email
+		Password: "TODO: harder",          // TODO: fun password
+		// FIXME: provide location
+		Birthday:   time.Date(2007, 10, 7, 0, 0, 0, 0, nil), // TODO: fun date (Feb?)
+		Address:    "Wherever",                              // TODO: fun address (Unicode? Zalgo?)
+		Phone:      nil,                                     // TODO: fun phone
+		Technology: "js",
+		Deleted:    nil,
+	},
+	{
+		Name: "Dorothy", Surname: "Deleted de 1",
+		Email:    "bobby.tables@xkcd.com",
+		Password: "",
+		// FIXME: provide location
+		Birthday:   time.Date(2000, 12, 12, 0, 0, 0, 0, nil),
+		Address:    "Dorothyville 1",
+		Phone:      newString("+48 1"),
+		Technology: "go",
+		// FIXME: provide location?
+		Deleted: newTime(time.Date(2020, 3, 28, 12, 21, 1, 0, nil)),
+	},
+	{
+		Name: "Dorothy", Surname: "Deleted de 2",
+		Email:    "bobby.tables@xkcd.com",
+		Password: "",
+		// FIXME: provide location
+		Birthday:   time.Date(2000, 12, 12, 0, 0, 0, 0, nil),
+		Address:    "Dorothyville 2",
+		Phone:      newString("+48 2"),
+		Technology: "go",
+		// FIXME: provide location?
+		Deleted: newTime(time.Date(2020, 3, 28, 12, 21, 2, 0, nil)),
+	},
+}
+
+func newString(v string) *string     { return &v }
+func newTime(v time.Time) *time.Time { return &v }
