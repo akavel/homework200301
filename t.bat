efbb4e129ea769e9af82a711b4c4c9e6fa14ab69
Mateusz Czapliński <czapkofan@gmail.com>
2020-03-28 13:55:09 +0100

    t.bat: add quick testing script

---
+++ t.bat
@@ -0,0 +1,16 @@
+@echo off
+goto :%1
+goto :eof
+
+:get
+@echo on
+curl -i http://localhost:8080/v1/user
+curl -i http://localhost:8080/v1/user/john@smith.name
+curl -i http://localhost:8080/v1/user/john@smith.nam
+curl -i http://localhost:8080/v1/user/jane@example.com
+@echo off
+goto :eof
+
+:post
+curl -i -XPOST -HContent-Type:application/json -d@testdata/jane.json http://localhost:8080/v1/user
+goto :eof
