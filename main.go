59136cee5ae855e38af70f7b8b5e5d6397f19693
Mateusz Czapliński <czapkofan@gmail.com>
2020-04-01 13:57:44 +0200

    move User to separate file
    Move User type to a separate file, in preparation for later commits
    adding more User-related contents.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/go-pg/pg/v9"
 	"github.com/gorilla/mux"
@@ -51,25 +50,6 @@ func main() {
 	r.Methods("PUT").Path("/v1/user/{id}").HandlerFunc(modifyUser)
 	r.Methods("DELETE").Path("/v1/user/{id}").HandlerFunc(deleteUser)
 	log.Fatal(http.ListenAndServe(*addr, r))
-}
-
-// FIXME: add `pg:"notnull"` stuff etc.
-// FIXME: make Technology an enum or FK in Postgres
-// TODO: [LATER] pg-related annotations & fields are internal and should not be exposed in exported type (see orm.Table?)
-type User struct {
-	// ID is the field required by go-pg ORM as the primary key
-	ID int64 `json:"-"`
-
-	Name    string `json:"name" pg:",notnull"`
-	Surname string `json:"surname" pg:",notnull"`
-	Email   string `json:"email" pg:",notnull"`
-	// FIXME: [LATER] only store a hash of the password
-	Password   string     `json:"password" pg:",notnull"`
-	Birthday   time.Time  `json:"birthday" pg:",notnull"`
-	Address    string     `json:"address" pg:",notnull"`
-	Phone      *string    `json:"phone",omitempty`
-	Technology string     `json:"technology" pg:",notnull"`
-	Deleted    *time.Time `json:"deleted,omitempty"`
 }
 
 // TODO: [LATER] introduce Context to methods, to allow timeouts control
