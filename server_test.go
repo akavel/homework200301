d770e7c7fa54fc5c23d2236f6c397adc7a4c5d3c
Mateusz Czapliński <czapkofan@gmail.com>
2020-04-03 13:01:16 +0200

    tests: add getUser test

--- server_test.go
+++ server_test.go
@@ -445,3 +445,84 @@ func dumpJSON(v interface{}) string {
 	buf, _ := json.Marshal(v)
 	return string(buf)
 }
+
+func TestServer_GetUser(t *testing.T) {
+	tests := []struct {
+		comment    string
+		email      string
+		mockResult *User
+		mockError  error
+		wantStatus int
+	}{
+		{
+			comment:    "existing user",
+			email:      "rick@black.name",
+			mockResult: &User{Email: newString("rick@black.name")},
+			wantStatus: http.StatusOK,
+		},
+		{
+			comment:    "no such user",
+			email:      "nobody@nowhere.gov",
+			mockResult: nil,
+			wantStatus: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		gotEmail := make(chan string, 1)
+		srv := Server{
+			DB: callbackDB{
+				getUser: func(email string) (*User, error) {
+					gotEmail <- email
+					// close chan to force panic if func called more than once
+					close(gotEmail)
+					return tt.mockResult, tt.mockError
+				},
+			},
+		}
+		r := mux.NewRouter()
+		srv.RegisterAt(r)
+		listener := httptest.NewServer(r)
+		client := listener.Client()
+
+		rs, err := client.Get(listener.URL + "/v1/user/" + tt.email)
+		if err != nil {
+			t.Errorf("%q: HTTP request error: %s", tt.comment, err)
+			listener.Close()
+			continue
+		}
+		// Verify HTTP status of response
+		if rs.StatusCode != tt.wantStatus {
+			t.Errorf("%q: want status %v, got %v (%v)", tt.comment, tt.wantStatus, rs.StatusCode, rs.Status)
+		}
+		// Verify body of response
+		body, err := ioutil.ReadAll(rs.Body)
+		rs.Body.Close()
+		listener.Close()
+		if err != nil {
+			t.Errorf("%q: error reading Body: %s", tt.comment, err)
+			continue
+		}
+		var gotUser *User
+		err = json.Unmarshal(body, &gotUser)
+		if len(body) > 0 && err != nil {
+			t.Errorf("%q: error unmarshalling response as JSON: %s, in:\n%s",
+				tt.comment, err, string(body))
+		}
+		if !reflect.DeepEqual(gotUser, tt.mockResult) {
+			t.Errorf("%q: bad response:\nwant: %s\nhave: %s\nraw:  %s",
+				tt.comment, dumpJSON(tt.mockResult), dumpJSON(gotUser), string(body))
+		}
+		// Verify email decoded from URL
+		select {
+		case e := <-gotEmail:
+			if e != tt.email {
+				t.Errorf("%q: bad decoded email:\nwant: %s\nhave: %s", tt.comment, tt.email, e)
+			}
+		default:
+			t.Errorf("%q: getUser not called", tt.comment)
+		}
+	}
+}
+
+func newString(v string) *string { return &v }
